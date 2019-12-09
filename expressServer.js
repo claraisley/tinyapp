d882730b18f4aa704b63e69eaa42ae7dd9ce3765
claraisley <isleyc97@gmail.com>
2019-12-09 22:20:12 +0000

    added a get to redirect the short url to the actual page of the long url

--- expressServer.js
+++ expressServer.js
@@ -20,26 +20,29 @@ function generateRandomString() {
   let i = 0;
   let ret='';
   while(i++ < anysize) {
-    ret += charset.charAt(Math.random() * charset.length)
+    ret += charset.charAt(Math.random() * charset.length);
   }
   return ret;
-}
-
+};
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get ("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase); 
   res.redirect(`/urls/${shortURL}`);      
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.get ("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
