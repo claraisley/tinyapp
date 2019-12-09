8372cce6b542fc0ea682450ef192b94e63a87836
claraisley <isleyc97@gmail.com>
2019-12-09 21:33:13 +0000

    Added generate rswndom string function

--- expressServer.js
+++ expressServer.js
@@ -4,13 +4,33 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let random = "";
+
+  for (let i = 0; i < alphabet.length; i++) {
+    random += alphabet.charAt(Math.random(6) * alphabet.length);
+  }
+  return random;
+}
+
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get ("/urls", (req, res) => {
